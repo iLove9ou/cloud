e2ffac0b9e39b6e705e37d42d8bb8d15314298d3
iLove9ou <624576640@qq.com>
2018-06-18 13:15:17 +0800

    Initial commit

---
+++ src/main/java/com/cloud/computing/cloud/CloudApplication.java
@@ -0,0 +1,14 @@
+package com.cloud.computing.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class CloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudApplication.class, args);
+    }
+}
