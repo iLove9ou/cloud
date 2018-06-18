7b8d02352e629d6a45108a4ae1bbc7cd482246a8
iLove9ou <624576640@qq.com>
2018-06-19 07:25:34 +0800

    It's OK, this is a milestone. services register on eureka server successfully!

--- coud-gateway/src/main/java/cloudgateway/coudgateway/CoudGatewayApplication.java
+++ coud-gateway/src/main/java/cloudgateway/coudgateway/CoudGatewayApplication.java
@@ -2,11 +2,10 @@ package cloudgateway.coudgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
-//@EnableEurekaClient
+@SpringBootApplication
+@EnableEurekaClient
 
 public class CoudGatewayApplication {
 
--- service/src/main/java/com/cloud/service/ServiceApplication.java
+++ service/src/main/java/com/cloud/service/ServiceApplication.java
@@ -2,10 +2,12 @@ package com.cloud.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableEurekaClient
+
 public class ServiceApplication {
 
     public static void main(String[] args) {
