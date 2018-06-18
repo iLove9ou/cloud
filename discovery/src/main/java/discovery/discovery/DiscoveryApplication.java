2b68263c67b5944114b3fc01006d0e3ba0186e12
iLove9ou <624576640@qq.com>
2018-06-18 23:27:14 +0800

    fix bug, add eureka client and server maven dependency on the same time

--- discovery/src/main/java/discovery/discovery/DiscoveryApplication.java
+++ discovery/src/main/java/discovery/discovery/DiscoveryApplication.java
@@ -2,11 +2,10 @@ package discovery.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-//import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
-//@EnableEurekaServer
+@SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryApplication {
 
     public static void main(String[] args) {
--- discovery/src/test/java/discovery/discovery/DiscoveryApplicationTests.java
+++ discovery/src/test/java/discovery/discovery/DiscoveryApplicationTests.java
@@ -3,9 +3,9 @@ package discovery.discovery;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
 @SpringBootTest
 public class DiscoveryApplicationTests {
 
